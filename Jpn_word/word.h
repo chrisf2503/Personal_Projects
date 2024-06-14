77fe8c132e15fe16267b0cb073a4278103dfba18
Christopher Flores <christopher321flores@gmail.com>
2024-06-14 15:34:55 -0400

    adding english meaning to the word class an adding another template

--- Jpn_word/word.h
+++ Jpn_word/word.h
@@ -1,30 +1,34 @@
 #pragma once
 #include <string>
 
-template<typename data>
+template<typename data1,typename data2>
 class word{
     public:
         //default
-        word(){}
-        word(data k, std::string s = ""){
+        word(data1 k = L' ', std::string s = "" ,data2 engl = ""){
             this->kanji_ = k;
             this->see_kanji_ = s;
+            this->engl_ = engl;
         }
-        void set_kanji(data k){
+        void set_kanji(data1 k){
             this->kanji_ = k;
         }
         //getting the value of the character
         size_t get_value(){
             return kanji_;
         }
-        data get_kanji(){
+        data1 get_kanji(){
             return this->kanji_;
         }
         std::string get_see_kanji(){
             return see_kanji_;
         }
+        data2 get_meaning(){
+            return this->engl_;
+        }
     private:
         //storing a wide chacater
-        data kanji_;
+        data1 kanji_;
         std::string see_kanji_;
+        data2 engl_;
 };
